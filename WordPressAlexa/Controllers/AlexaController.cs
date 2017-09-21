42cd5fc7fcc0cfe66ab7ac6eff1fc5478acd23ce
Thomas Pentenrieder <pentenrieder@medialesson.de>
2017-09-21 09:55:54 +0200

    read latest post

--- WordPressAlexa/Controllers/AlexaController.cs
+++ WordPressAlexa/Controllers/AlexaController.cs
@@ -1,17 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using WordPressPCL;
 using Microsoft.Extensions.Configuration;
-using System.Diagnostics;
 using Alexa.NET.Request;
 using Alexa.NET.Request.Type;
 using Alexa.NET.Response;
 using Alexa.NET;
 using System.Text;
+using WordPressPCL.Utility;
+using WordPressAlexa.Utility;
 
 namespace WordPressAlexa.Controllers
 {
@@ -58,24 +56,67 @@ namespace WordPressAlexa.Controllers
             // check the name to determine what you should do
             if (intentRequest.Intent.Name.Equals("Headlines"))
             {
-                var posts = await _client.Posts.Get();
-                StringBuilder sb = new StringBuilder();
-                sb.Append("<speak>Hier die Schlagzeilen.<break time=\"1s\"/>");
-                // build the speech response 
-                for (int i = 0; i < 5; i++)
-                {
-                    sb.Append($"{posts.ElementAt(i).Title.Rendered}<break time=\"1s\"/>");
-                }
-                sb.Append("</speak>");
+                var sb = await GetHeadlines();
                 speech.Ssml = sb.ToString();
 
                 // create the response using the ResponseBuilder
                 var finalResponse = ResponseBuilder.Tell(speech);
                 return finalResponse;
-            } else
+            }
+            else if (intentRequest.Intent.Name.Equals("LatestPost"))
+            {
+                var sb = await GetLatestPost();
+                speech.Ssml = sb.ToString();
+                var finalResponse = ResponseBuilder.Tell(speech);
+                return finalResponse;
+            }
+            else
             {
                 return ErrorResponse();
             }
+        }
+
+        private async Task<StringBuilder> GetLatestPost()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var latestPosts = await _client.Posts.Query(new WordPressPCL.Utility.PostsQueryBuilder()
+            {
+                Page = 1,
+                PerPage = 1
+            });
+            var post = latestPosts.FirstOrDefault();
+            if(post != null)
+            {
+                var content = Helpers.ScrubHtml(post.Content.Rendered);
+                var title = Helpers.ScrubHtml(post.Title.Rendered);
+                sb.Append($"<speak>{title}<break time=\"1s\"/>{content}</speak>");
+            }
+            else
+            {
+                sb.Append("<speak>Irgendwas ist schiefgelaufen.</speak>");
+            }
+            return sb;
+
+        }
+
+        private async Task<StringBuilder> GetHeadlines()
+        {
+            var posts = await _client.Posts.Get();
+            StringBuilder sb = new StringBuilder();
+            sb.Append("<speak>Hier die Schlagzeilen.<break time=\"1s\"/>");
+            // build the speech response 
+            for (int i = 0; i < 5; i++)
+            {
+                sb.Append($"{posts.ElementAt(i).Title.Rendered}");
+                if (i == 4)
+                {
+                    sb.Append(".");
+                }
+                sb.Append("<break time=\"1s\"/>");
+            }
+            sb.Append("</speak>");
+            return sb;
         }
 
         private SkillResponse ErrorResponse()
---
+++ WordPressAlexa/Utility/Helpers.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+namespace WordPressAlexa.Utility
+{
+    public static class Helpers
+    {
+        public static string ScrubHtml(string value)
+        {
+            var step1 = Regex.Replace(value, @"<[^>]+>|&nbsp;", "").Trim();
+            var step2 = Regex.Replace(step1, @"\s{2,}", " ");
+            return step2;
+        }
+    }
+}
